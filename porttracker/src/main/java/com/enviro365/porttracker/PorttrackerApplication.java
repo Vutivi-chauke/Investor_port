1ab13d24b404d3c028d1978b1fd191e67757f926
Vutivi-chauke <vutivijajamba@gmail.com>
2026-08-18 00:23:46 +0200

    Add files via upload

---
+++ porttracker/src/main/java/com/enviro365/porttracker/PorttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.enviro365.porttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorttrackerApplication.class, args);
+	}
+
+}
---
+++ porttracker/src/test/java/com/enviro365/porttracker/PorttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro365.porttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
